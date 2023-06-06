ac4e31af7c55dc06bf3ed2d775314c95301a8ed4
Rijul_Sharma <rijul.sharma@tech-bridge.biz>
2023-06-06 16:13:50 +0530

    initial commit xampp xxwarmachineroxx 6 June 2023 3 hours of work

---
+++ application/logs/log-2023-06-06.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-06-06 15:45:47 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 15:47:21 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 15:51:36 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 15:51:45 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 15:59:12 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 15:59:18 --> 404 Page Not Found: Frontend/images
+ERROR - 2023-06-06 16:00:19 --> 404 Page Not Found: Frontend/images
---
+++ application/views/includes/api-banner.php
@@ -0,0 +1,49 @@
+<div class="api-banner-section">
+<div class="container">
+<div class="row">
+<div class="col-12 col-md-7 order-1 order-md-0">
+    <?php include('form.php'); ?> 
+</div>
+<div class="col-12 col-md-5 align-self-end order-0">
+    <div class="text-center px-2 px-md-3 d-none">
+            <h1 class="h3 form-heading">Search, Compare & Book Air Tickets at Best Price Instantly.</h1>
+         </div>
+		 <div class="banner-right clear clearfix mb-lg-4 mt-0 mt-md-3 mx-0 mx-md-3 mx-md-5">
+        <h2><span class="txt01">Explore the world with </span> <strong><span class="airline-name">Airfaremoss</span> </strong><span class="txt02"></span></h2>
+                        <div>
+                           <div id="banner01" class="owl-carousel owl-theme">				
+                 <div class="item item-ctm">				
+                   <p>Group Travel Deals - <span>SAVE UPTO 30%</span></p>
+                 </div>
+				 <div class="item item-ctm">				
+                   <p>Last-minute Flight Deals - <span>Get cheap last minutes flights</span></p>
+                 </div>
+				 <div class="item item-ctm">				
+                   <p>Call us for urgent - <span class="tcsv">
+<i class="fa fa-check-circle" aria-hidden="true"></i> Booking&nbsp;&nbsp;  
+<i class="fa fa-check-circle" aria-hidden="true"></i> Changes&nbsp;&nbsp;
+<i class="fa fa-check-circle" aria-hidden="true"></i> Cancellation&nbsp;&nbsp; 
+<i class="fa fa-check-circle" aria-hidden="true"></i> Assistance
+</span></p>
+</div>	 
+</div>
+                        </div>
+        </div>
+		
+
+		
+</div>
+
+
+</div>
+</div>
+</div>
+   
+   
+   
+   
+   
+   
+   
+   
+   
