7de33230ea4866a679cc6500a8ba4da123514d4a
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2023-08-21 14:18:37 +0530

    added 5 new images for aeromexico, cathay pacific , ethihad , ethihad  , hawaian airlines , latam

--- application/views/hawaiian-airlines.php
+++ application/views/hawaiian-airlines.php
@@ -3,7 +3,7 @@ document.title = "Fly farther for less - discover low cost flights with Hawaiian
 document.getElementsByTagName('meta')["keywords"].content = "";       
 document.getElementsByTagName('meta')["description"].content = "Find cheap flight at Airfaremoss. Get the best fairs With US - call us @ <?= TFN ?>";   
 </script>
-   <div id="header-wrapper" class="wrap-inpg py-4 py-md-5"  style="background-image: url('frontend/images/hawaiian-air.jpg'); background-position: bottom;background-no-repeat: no-repeat; background-size: cover;">
+   <div id="header-wrapper" class="wrap-inpg py-4 py-md-5"  style="background-image: url('frontend/images/hawaiian-airlines.jpg'); background-position: bottom;background-no-repeat: no-repeat; background-size: cover;">
  <?php include 'includes/api-banner.php'; ?>  
 </div>
 <div class="whysky mb-4 mb-md-5">
